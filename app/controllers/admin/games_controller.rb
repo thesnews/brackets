979eba467e3216d1b489c6718eda5e900ea21ae5
Greg <gmlyons14@gmail.com>
2016-03-08 00:31:54 -0500

    Added admin games controller for edit/update, show games in admin tournament view

---
+++ app/controllers/admin/games_controller.rb
@@ -0,0 +1,32 @@
+class Admin::GamesController < ApplicationController
+  layout 'admin'
+
+  def edit
+    @game = Game
+      .includes(:tournament)
+      .find(params[:id])
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    if @game.update_attributes(game_params)
+      flash[:success] = "Your game has been updated."
+      redirect_to [:admin, @game.tournament]
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(
+      :team1_id,
+      :team2_id,
+      :score1,
+      :score2,
+      :start_time,
+      :final
+    )
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :tournaments do
       resources :teams, only: [:new, :create, :edit, :update]
+      resources :games, only: [:edit, :update]
     end
   end
 
